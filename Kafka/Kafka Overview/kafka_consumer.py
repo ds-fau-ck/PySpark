8fd62abf3aebb608ccb410c7ea720ea632cd9b9c
DS-FAU <42653001+ds-fau-ck@users.noreply.github.com>
2024-07-26 12:06:31 +0200

    Update kafka_consumer.py

--- Kafka/Kafka Overview/kafka_consumer.py
+++ Kafka/Kafka Overview/kafka_consumer.py
@@ -1,1 +1,70 @@
+import threading
+from confluent_kafka import DeserializingConsumer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroDeserializer
+from confluent_kafka.serialization import StringDeserializer
 
+
+
+# Define Kafka configuration
+kafka_config = {
+    'bootstrap.servers': 'pkc-56d1g.eastus.aws.confluent.cloud:9092',
+    'sasl.mechanisms': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': '4MTJOS5G7HKCLY7W',
+    'sasl.password': 'jdf/WNotpjtbpOlZbOaAXKwKiVtwSLZEbEg5YN1+3x7JEKpHQKuA0hK+aBqZY1bh',
+    'group.id': 'group1',
+    'auto.offset.reset': 'earliest'
+}
+
+# 'group.id': 'group1',
+
+# Create a Schema Registry client
+schema_registry_client = SchemaRegistryClient({
+  'url': 'https://psrc-rrzpk0.eastus.aws.confluent.cloud',
+  'basic.auth.user.info': '{}:{}'.format('CLTYBNLBMCYEJ47V', 'J3gjYdknh7u4++fk3BvuGkhbl+hegRLcmArOYINX7vmxzfRPtXQ1KyUUJ2Ga818v')
+})
+
+# Fetch the latest Avro schema for the value
+subject_name = 'retail_data_test-value'
+schema_str = schema_registry_client.get_latest_version(subject_name).schema.schema_str
+
+# Create Avro Deserializer for the value
+key_deserializer = StringDeserializer('utf_8')
+avro_deserializer = AvroDeserializer(schema_registry_client, schema_str)
+
+# Define the DeserializingConsumer
+consumer = DeserializingConsumer({
+    'bootstrap.servers': kafka_config['bootstrap.servers'],
+    'security.protocol': kafka_config['security.protocol'],
+    'sasl.mechanisms': kafka_config['sasl.mechanisms'],
+    'sasl.username': kafka_config['sasl.username'],
+    'sasl.password': kafka_config['sasl.password'],
+    'key.deserializer': key_deserializer,
+    'value.deserializer': avro_deserializer,
+    'group.id': kafka_config['group.id'],
+    'auto.offset.reset': kafka_config['auto.offset.reset']
+    # 'enable.auto.commit': True,
+    # 'auto.commit.interval.ms': 5000 # Commit every 5000 ms, i.e., every 5 seconds
+})
+
+# Subscribe to the 'retail_data' topic
+consumer.subscribe(['retail_data_test'])
+
+#Continually read messages from Kafka
+try:
+    while True:
+        msg = consumer.poll(1.0) # How many seconds to wait for message
+
+        if msg is None:
+            continue
+        if msg.error():
+            print('Consumer error: {}'.format(msg.error()))
+            continue
+
+        print('Successfully consumed record with key {} and value {}'.format(msg.key(), msg.value()))
+
+except KeyboardInterrupt:
+    pass
+finally:
+    consumer.close()
